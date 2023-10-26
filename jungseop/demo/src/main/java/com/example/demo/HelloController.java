b5b11e2409d64a5cafe5eff7434dd980fc6b75c0
황중섭 <jshwang9709@gmail.com>
2023-10-26 15:22:37 +0900

    [junseop] 1주차 과제 완료

--- jungseop/demo/src/main/java/com/example/demo/HelloController.java
+++ jungseop/demo/src/main/java/com/example/demo/HelloController.java
@@ -1,28 +1,22 @@
 package com.example.demo;
 
-import com.example.demo.todo.Content;
-import com.example.demo.todo.Todo;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-@Controller
+@RestController
 public class HelloController {
 
-    @ResponseBody
-    @GetMapping("/todo")
-    public Todo todoApi() {
-        List<Content> contents = new ArrayList<>();
-        contents.add(new Content("백엔드 개발중 입니다.", "시작 전"));
-        contents.add(new Content("백엔드 개발중 입니다..", "진행중"));
-        contents.add(new Content("백엔드 개발중 입니다...", "완료"));
-        Todo todo = new Todo(contents);
+    @GetMapping("/list")
+    public PeopleDto personList() {
+        List<Person> list = new ArrayList<>();
+        list.add(new Person("Spring1", 15, "Seoul1", "Developer1"));
+        list.add(new Person("Spring2", 16, "Seoul2", "Developer2"));
+        list.add(new Person("Spring3", 17, "Seoul3", "Developer3"));
 
-        return todo;
+        return new PeopleDto(list);
     }
+
 }
